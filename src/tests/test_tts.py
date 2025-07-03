7781e77678b398e3b8c7413a37bb88d370a2a07e
DmitriiK <dklmn@mail.ru>
2025-07-03 15:49:55 +0300

    ssml tts

--- src/tests/test_tts.py
+++ src/tests/test_tts.py
@@ -1,16 +1,27 @@
 import unittest
+import os
 from src.tts.tts_generator import TTS_GEN
 
+OUTPUT_DIR = 'src/tests/test_data/outputs/audio'
 class TestTTS(unittest.TestCase):
 
     def test_turkish(self):
         #  https://learn.microsoft.com/en-us/azure/ai-services/speech-service/language-support?tabs=tts
         #  text = "Hello! 你好! Hola! नमस्ते! Bonjour! こんにちは! مرحبا! 안녕하세요! Ciao! Cześć! Привіт! வணக்கம்!"
         text = """Ben bunu bilmiyordum. Hava çok sıcak oldu."""
+        output_file_name = os.path.join(OUTPUT_DIR, 'test_tts')
         tts = TTS_GEN('tr-TR-EmelNeural')
-        tts.generate_audio(text, 'test_tts')
+        tts.generate_audio(input_tts=text, output_file_name=output_file_name, is_ssml=False, skip_if_exists=False)
+
+    def test_ssml(self):
+        # Test generating audio from SSML using the example from SSML.xml
+        with open('src/tests/test_data/outputs/SSML.xml', 'r', encoding='utf-8') as f:
+            ssml = f.read()
+        tts = TTS_GEN()
+        output_file_name = os.path.join(OUTPUT_DIR, 'test_ssml')
+        tts.generate_audio(input_tts=ssml, output_file_name=output_file_name, is_ssml=True, skip_if_exists=False)
 
 if __name__ == '__main__':
     ttts = TestTTS()
     ttts.test_turkish()
-    
+    ttts.test_ssml()
--- src/tts/tts_generator.py
+++ src/tts/tts_generator.py
@@ -8,6 +8,7 @@ import azure.cognitiveservices.speech as speechsdk
 
 from src import config as cfg
 
+logging
 
 class TTS_GEN:
     def __init__(self,
@@ -39,21 +40,37 @@ class TTS_GEN:
                 return
             return sex_voices[0]
 
-    def generate_audio(self, tts: str, file_name: str = '', skip_if_exists=False):
-        file_name = file_name or self.voice
-        file_name = file_name + '.mp3'
+    def generate_audio(self, input_tts: str, 
+                       is_ssml: bool = False, 
+                       output_file_name: str = '', 
+                       skip_if_exists=False):
+        """
+        Generates audio from the provided text or SSML input using the configured speech synthesizer.
+        Args:
+            input_tts (str): The text or SSML input to synthesize into audio.
+            is_ssml (bool, optional): If True, treats input_tts as SSML. If False, treats as plain text. Defaults to False.
+            output_file_name (str, optional): The name of the output audio file. If not provided, uses the voice name with '.mp3' extension. Defaults to ''.
+            skip_if_exists (bool, optional): If True and the output file already exists, skips audio generation. Defaults to False.
+        Returns:
+            None
+        Side Effects:
+            - Writes the synthesized audio to the specified file.
+            - Logs information and warnings about the synthesis process.
+            - Prints error details if synthesis is canceled due to an error.
+        """
+        output_file_name = (output_file_name or self.voice) + '.mp3'
         if self.our_dir_path:
-            file_name = os.path.join(self.our_dir_path, file_name)
-        if os.path.exists(file_name) and skip_if_exists:
-            logging.warning(f'file {file_name} exists. skipping')
+            output_file_name = os.path.join(self.our_dir_path, output_file_name)
+        if os.path.exists(output_file_name) and skip_if_exists:
+            logging.warning(f'file {output_file_name} exists. skipping')
             return
-        logging.info(f'producing audio for text having len {len(tts)} chars')
-        file_config = speechsdk.audio.AudioOutputConfig(filename=file_name)
-        speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=self.speech_config, audio_config=file_config)
-
-        result = speech_synthesizer.speak_text_async(tts).get()
+        logging.info(f'producing audio for text having len {len(input_tts)} chars')
+        audio_config = speechsdk.audio.AudioOutputConfig(filename=output_file_name)
+        speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=self.speech_config, audio_config=audio_config)
+        tts_method = speech_synthesizer.speak_ssml_async if is_ssml else speech_synthesizer.speak_text_async
+        result = tts_method(input_tts).get()
         if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-            logging.info(f'wrote audio to file {file_name}.')
+            logging.info(f'wrote audio to file {output_file_name}.')
         elif result.reason == speechsdk.ResultReason.Canceled:
             cancellation_details = result.cancellation_details
             print("Speech synthesis canceled: {}".format(cancellation_details.reason))
