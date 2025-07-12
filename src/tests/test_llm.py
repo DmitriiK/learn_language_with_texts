570b335c88362c85ece02cb2e85fd613a48b57a8
DmitriiK <dklmn@mail.ru>
2025-07-12 14:39:57 +0300

    test for number of questions

--- src/tests/test_llm.py
+++ src/tests/test_llm.py
@@ -7,7 +7,10 @@ class TestLLM(TestCase):
             input_text = file.read()
 
         target_language = "Russian"
-        result = create_bilingual_text(input_text, target_language, number_of_questions=3)
+        number_of_questions=3
+        result = create_bilingual_text(input_text, target_language, number_of_questions=number_of_questions
+                                       )
         assert len(result.paragraphs) > 0, "Result should contain at least one paragraph"
+        assert len(result.questions) == number_of_questions, f"Expected {number_of_questions} questions, got {len(result.questions)}"
         assert result.source_language in ["Turkish", 'tr', 'tr-TR'], "Source language should be Turkish"
         print(result.to_yaml())
